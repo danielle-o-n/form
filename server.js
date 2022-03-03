81f2103f28d0759ade153c6d0d2bfe9db16f766b
Danielle Oliveira <danielle.o.n@hotmail.com>
2022-03-03 10:13:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ nunjucks.configure("view", {
 })
 
 
+const PORT = process.env.PORT || 5000;
 
-servidor.listen(5000,()=>{
-    console.log("server is run");
+servidor.listen(PORT, ()=>{
+    console.log("A PORTA e" + PORT);
 })
 
